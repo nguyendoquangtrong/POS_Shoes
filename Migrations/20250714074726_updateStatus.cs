da5f0c7a65ee9126ccc2b06849f0fc2929d33692
Nguyễn Trọng <trongnguyen9112005@gmail.com>
2025-07-14 14:48:20 +0700

    updateStatus

---
+++ Migrations/20250714074726_updateStatus.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POS_Shoes.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "ReturnReceipts",
+                newName: "Status");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Quantity",
+                table: "ReturnReceipts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "ReturnReceipts");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "ReturnReceipts",
+                newName: "status");
+        }
+    }
+}
--- obj/Debug/net9.0/POS_Shoes.AssemblyInfo.cs
+++ obj/Debug/net9.0/POS_Shoes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS_Shoes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e06ca1d5b88781932699a3e1bd8418392df5665")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS_Shoes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS_Shoes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
